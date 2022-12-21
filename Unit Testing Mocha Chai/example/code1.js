430784ab5dbe86b88ea219ebe7f267a6e25a4342
Uriel Ofir <urielofir@gmail.com>
2022-12-21 19:39:43 +0200

    Correcting a typo mistake

--- Unit Testing Mocha Chai/example/code1.js
+++ Unit Testing Mocha Chai/example/code1.js
@@ -1,6 +1,6 @@
 // Intentionally blank file.
 // 
 // This is what you will see in a production 
-// enviroment where you want to macke sure that
+// enviroment where you want to make sure that
 // you have a structure "clean" way of finding
 // your code. 
